bf93e913229a92c78e94d06de52967f4feb6d0c4
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-03-03 19:08:41 +0530

    Update main.js

--- prime number checker/main.js
+++ prime number checker/main.js
@@ -3,6 +3,11 @@ function prime() {
    let flag = 0;
    number = Number(document.getElementById("num").value);
    if (number){
+      if (number <= 0){
+         document.getElementById("ans").innerHTML = `${number} is netiher a Prime nor Composite number`;
+         return
+      }
+
       if (number == 1){
          document.getElementById("ans").innerHTML = `${number} is netiher a Prime nor Composite number`;
          return
