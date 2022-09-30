938910d41035e9c0d0bd56b6bef0ec3e6d68d290
Sai Prasad Wali <84082731+saiwali@users.noreply.github.com>
2022-10-01 01:16:49 +0530

    Add files via upload

---
+++ Number guessing game/index.js
@@ -0,0 +1,10 @@
+
+var b = prompt(" enter a random number ")
+var a = Math.floor(Math.random()*9 + 1)
+
+
+if ( a === b){
+  alert(" congratulations u have guessed the number currenty")
+}else{
+  alert(" Incorrect --> try again")
+}
